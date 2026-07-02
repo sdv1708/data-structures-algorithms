fe3d9d49a516e224e7d7960dfd3bf6c22ebabc68
Sanjay DV <55549923+sdv1708@users.noreply.github.com>
2026-07-02 01:57:12 -0500

    Add: island-perimeter - submission-2

---
+++ Data Structures & Algorithms/island-perimeter/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        per = 0
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c] == 1:
+                    per += 4 
+                    if r > 0 and grid[r - 1][c] == 1:
+                        per -= 2
+                    if c > 0 and grid[r][c - 1] == 1:
+                        per -= 2
+        return per
